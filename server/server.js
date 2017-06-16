2cd6484bf906f7e025edeb5f50424b225af929ff
kaushik <itiskaushikofficial@gmail.com>
2017-06-16 23:09:46 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+var publicPath = path.join(__dirname, '../public');
+app.use(express.static(publicPath));
+
+
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
+});
