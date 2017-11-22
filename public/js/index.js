e22c391b0b27c881441d5593f0dc026ad9a24e97
kaushik <itiskaushikofficial@gmail.com>
2017-11-22 12:56:07 +0530

    Configured templates for messages

--- public/js/index.js
+++ public/js/index.js
@@ -10,19 +10,27 @@
   });
 
   socket.on('newMessage', function(message) {
-      var formattedTime = moment(message.createdAt).format('LT');
-      var li = jQuery('<li></li>');
-      li.text(`${message.from} ${formattedTime} : ${message.text}`);
-      jQuery('#messages').append(li);
+        var formattedTime = moment(message.createdAt).format('LT');      
+        var template = jQuery('#message-template').html();
+        var html = Mustache.render(template,{
+            text: message.text,
+            from: message.from,
+            createdAt: formattedTime
+        });
+
+        jQuery('#messages').append(html);
   });
+
   socket.on('newLocationMessage', function(message){
-    var formattedTime = moment(message.createdAt).format('LT');
-    var li = $('<li></li>');
-    var a = $('<a target="_blank">My Current Location</a>');
-    li.text(`${message.from} ${formattedTime} : `);
-    a.attr('href',message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
+    var formattedTime = moment(message.createdAt).format('LT');      
+    var template = jQuery('#location-message-template').html();
+    
+    var html = Mustache.render(template,{
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
+        jQuery('#messages').append(html);
   });
 
 
